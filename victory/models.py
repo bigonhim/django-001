25ec80a9c6e9795b9553c321e0f729613bae668f
Biegon <156933863+Bigoncode@users.noreply.github.com>
2024-02-07 21:40:15 +0300

    vercel

--- victory/models.py
+++ victory/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
-    name = models.CharField(max_length=100)
-    Email_Address = models.EmailField(max_length=100)
-
-    def __str__ (self):
-        return self.name
-    
-class ToDoList(models.Model):
-    Person = models.ForeignKey(Person,on_delete=models.CASCADE)
-    Text = models.CharField(max_length=300) 
-
-    def __str__ (self):
-        return self.Text
